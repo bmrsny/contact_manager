74abd28fc26b5a98c461a6e98431c34150d9c201
Brandon Mrsny <bmrsny@gmail.com>
2015-01-07 10:14:45 -0700

    passing controller tests for email_addresses

---
+++ spec/models/email_address_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe EmailAddress, :type => :model do
+  let(:email_address) do
+    EmailAddress.new(address: 'geoff@geoff.com', person_id: 1)
+  end
+
+  it 'is valid' do
+    expect(email_address).to be_valid
+  end
+
+  it 'is invalid without an address' do
+    email_address.address = nil
+    expect(email_address).to_not be_valid
+  end
+end
