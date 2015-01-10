59d42ed0de2fae8264107d8c95f8384e54c1e6a4
Brandon Mrsny <bmrsny@gmail.com>
2015-01-10 13:02:38 -0700

    finished model spec for company

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -13,10 +13,12 @@ RSpec.describe Company, :type => :model do
   end
 
   it 'has an array of phone numbers'do
-    expect(company.phone_numbers).to eq([])
+    phone_number = company.phone_numbers.build(number: "333-4444")
+    expect(phone_number.number).to eq('333-4444')
   end
 
   it 'has an array of email addresses' do
-    expect(company.email_addresses).to eq([])
+    email_address = company.email_addresses.build(address: "e@e.com")
+    expect(email_address.address).to eq("e@e.com")
   end
 end
